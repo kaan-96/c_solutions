60582046ee7bad5e3d1689d41256e3435547f2a1
kaan-96 <kawa.annani@gmail.com>
2022-05-05 14:44:32 +0200

    latest

---
+++ chapter-2/projects/2.8/2.8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float loan, interest, monthly, balance;
+
+    printf("Enter amount of loan: ");
+    scanf("%f", &loan);
+
+    printf("Enter interest rate: ");
+    scanf("%f", &interest);
+  
+    printf("Enter monthly payment: ");
+    scanf("%f", &monthly);
+    
+    interest = 1.0f + ((interest / 12.0f) / 100.0f);
+
+    balance = loan * interest - monthly;
+    printf("\nBalance remaining after first payment: $%f\n", balance);
+
+    balance = balance * interest - monthly;
+    printf("Balance remaining after second payment: $%f\n", balance);
+
+    balance = balance * interest - monthly;
+    printf("Balance remaining after third payment: $%f\n", balance);
+
+    return 0;
+}
