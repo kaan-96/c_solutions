f88d1e2188f6ce6061a29125b3543e74d8cf0130
kaan-96 <kawa.annani@gmail.com>
2022-05-05 16:22:51 +0200

    latest

--- chapter-2/projects/7/07.c
+++ chapter-2/projects/7/07.c
@@ -10,15 +10,12 @@ int main(void)
     printf("$20 bills: %d\n", n / 20);
 
     n = n - 20 * (n / 20);
-
     printf("$10 bills: %d\n", n / 10);
 
     n = n - 10 * (n / 10);
-
     printf(" $5 bills: %d\n", n / 5);
 
     n = n - 5 * (n / 5);
-
     printf(" $1 bills: %d", n);
 
     return 0;
