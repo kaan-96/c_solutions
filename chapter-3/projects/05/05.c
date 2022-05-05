0af22183211898f7e8c4be90e1b85c8528a2df51
kaan-96 <kawa.annani@gmail.com>
2022-05-05 23:40:13 +0200

    latest

--- chapter-3/projects/05/05.c
+++ chapter-3/projects/05/05.c
@@ -7,7 +7,7 @@ int main(void)
     printf("Enter the numbers from 1 to 16 in any order: ");
     scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &a1, &a2, &a3, &a4, &b1, &b2, &b3, &b4, &c1, &c2, &c3, &c4, &d1, &d2, &d3, &d4);
 
-    printf("%d %d %d %d\n%d %d %d %d\n%d %d %d %d\n%d %d %d %d\n\n", a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4);
+    printf("%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n%2d %2d %2d %2d\n\n", a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4);
     printf("Row sums: %d %d %d %d\n", (a1 + a2 + a3 + a4), (b1 + b2 + b3 + b4), (c1 + c2 + c3 + c4), (d1 + d2 + d3 + d4));
     printf("Column sums: %d %d %d %d\n", (a1 + b1 + c1 + d1), (a2 + b2 + c2 + d2), (a3 + b3 + c3 + d3), (a4 + b4 + c4 + d4));
     printf("Diagonal sums: %d %d", (a1 + b2 + c3 + d4), (a4 + b3 + c2 + d1));
