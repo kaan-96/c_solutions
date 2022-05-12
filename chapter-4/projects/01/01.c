63d5803684cc3368d9a96a7be7b1a890bd3a72ed
kaan-96 <kawa.annani@gmail.com>
2022-05-12 15:42:39 +0200

    latest

---
+++ chapter-4/projects/01/01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int d;
+
+    printf("Enter a two-digit number: ");
+    scanf("%2d", &d);
+
+    printf("The reversal is: %d%d", d % 10, d / 10);
+
+    return 0;
+}
