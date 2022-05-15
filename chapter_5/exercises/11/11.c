8adb64624205d29ead9a98ee5f6420902a6099bb
kaan-96 <kawa.annani@gmail.com>
2022-05-15 14:56:58 +0200

    latest

--- chapter_5/exercises/11/11.c
+++ chapter_5/exercises/11/11.c
@@ -3,15 +3,30 @@
 int main(void) {
     int area_code;
 
+    // Scan request added for flavor.
+    printf("Enter area code: ");
+    scanf("%d", &area_code);
+
     switch (area_code) {
-
-
-
-        default: printf("Area code not recognized.");
-                break;
+        case 229:
+            printf("Albany");
+            break;
+        case 404: case 470: case 678: case 770:
+            printf("Atlanta");
+            break;
+        case 478:
+            printf("Macon");
+            break;
+        case 706: case 762:
+            printf("Columbus");
+            break;
+        case 912:
+            printf("Savannah");
+            break;
+        default:
+            printf("Area code not recognized.");
+            break;
     }
-
-
 
     return 0;
 }
