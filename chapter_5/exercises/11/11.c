4758f597f6e99535fe6a60deb3e129c9f6b3bdec
kaan-96 <kawa.annani@gmail.com>
2022-05-15 14:44:34 +0200

    latest

---
+++ chapter_5/exercises/11/11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int area_code;
+
+    switch (area_code) {
+
+
+
+        default: printf("Area code not recognized.");
+                break;
+    }
+
+
+
+    return 0;
+}
