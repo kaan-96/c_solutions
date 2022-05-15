fbbc8c5aa70638d809b166d2ff4bbd3d1651ba9f
kaan-96 <kawa.annani@gmail.com>
2022-05-15 20:18:49 +0200

    latest

--- chapter_5/projects/02/02.c
+++ chapter_5/projects/02/02.c
@@ -7,6 +7,8 @@ int main(void) {
     printf("Enter a 24-hour time: ");
     scanf("%d:%d", &hour, &minute);
 
+    printf("Equivalent 12-hour time: ");
+
     if (hour < 12)
 	    printf("%d:%.2d AM", hour == 0 || 24 ? 12 : hour, minute);
     else
